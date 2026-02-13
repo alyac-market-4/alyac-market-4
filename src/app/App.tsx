dc61538386014931cec839287543462b6eb4e184
sungmin jung <sungminjung066@gmail.com>
2026-02-13 16:41:27 +0900

    라우팅 설정 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,10 @@
+import { RouterProvider } from 'react-router-dom';
+
 import './App.css';
+import { router } from './routes';
 
 function App() {
-  return (
-    <div className="bg-background text-foreground min-h-screen">
-      <h1 className="py-10 text-center text-4xl font-bold">Alyac Market</h1>
-      <p className="text-muted-foreground text-center">
-        React + TypeScript + Vite 프로젝트가 성공적으로 설정되었습니다! 🎉
-      </p>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/app/routes.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import { HomePage } from '@/pages/home';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    // element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
