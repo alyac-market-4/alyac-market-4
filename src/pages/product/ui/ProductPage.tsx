0f60b6061051535351473828a244ec6b5d7e617a
yeonaa95 <dusghk014@naver.com>
2026-02-19 18:08:16 +0900

    feat: ProductPage에 맞는 Header 구성 수정

--- src/pages/product/ui/ProductPage.tsx
+++ src/pages/product/ui/ProductPage.tsx
@@ -1,17 +1,21 @@
-import { Search } from 'lucide-react';
+import { useState } from 'react';
 
-import { IconButton } from '@/shared/ui/IconButton';
+import { ImagePlus } from 'lucide-react';
+
+import { BackButton } from '@/shared/ui/BackButton';
+import { Button } from '@/shared/ui/button';
 import { Header } from '@/widgets/header';
 
 export const ProductPage = () => {
   return (
     <>
+      {/* 헤더 */}
       <Header
-        left="알약마켓 상품 등록"
+        left={<BackButton />}
         right={
-          <IconButton>
-            <Search />
-          </IconButton>
+          <Button className="rounded-full bg-green-600 px-6 text-white hover:bg-green-700">
+            저장
+          </Button>
         }
       />
       <div>상품 등록 페이지</div>
