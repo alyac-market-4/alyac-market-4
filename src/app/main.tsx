d841821da758d78c30074f1c9a721380397694dd
sungmin jung <sungminjung066@gmail.com>
2026-02-19 13:08:00 +0900

    shadcn 설정 추가

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
 
+import { Button } from '@/shared/ui/button.tsx';
+
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Button variant="outline" size="lg">
+      Hello World
+    </Button>
   </StrictMode>,
 );
