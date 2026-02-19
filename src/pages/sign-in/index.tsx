a3e2bb101f428dc9231df5efa2853244626ce60e
Catailog <tlsdudghks11@gmail.com>
2026-02-19 20:53:59 +0900

    test: sign-in 임시 토큰에서 임시 로그인으로 변경

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,14 +1,15 @@
 import { useNavigate } from 'react-router-dom';
 
-import { saveToken } from '@/shared/lib';
+import { useAuth } from '@/entities/auth';
 import { Button } from '@/shared/ui';
 
 export const SignInPage = () => {
   const navigate = useNavigate();
+  const { signInMutation } = useAuth();
 
   const onClick = () => {
-    // TODO: 임시 토큰을 실제 로그인 API 호출로 변경
-    saveToken('1', '1');
+    // TODO: 임시 로그인
+    signInMutation.mutate({ user: { email: 'test@test.com', password: '11111111' } });
     navigate('/feed');
   };
 
