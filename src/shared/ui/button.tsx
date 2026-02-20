f283f9fd19f72dc414fca93dc56bcd7d5762d13e
wodud2626 <wodud2626@gmail.com>
2026-02-20 21:49:26 +0900

    feat: homepage 페이지 전환 효과 구현. 버튼에 커서 포인터 추가.

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -6,7 +6,7 @@ import { Slot } from 'radix-ui';
 import { cn } from '@/shared/lib';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex cursor-pointer items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
@@ -19,7 +19,7 @@ const buttonVariants = cva(
         ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
         alyac:
-          'cursor-pointer transition-colors items-center justify-center gap-2 rounded-full bg-[#6FCA3C]  inline-flex text-white hover:bg-[#5CB32A]',
+          'transition-colors items-center justify-center gap-2 rounded-full bg-[#6FCA3C]  inline-flex text-white hover:bg-[#5CB32A]',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
