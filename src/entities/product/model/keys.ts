75e0a606985b5e585f386ac185314496d519cb9d
Catailog <tlsdudghks11@gmail.com>
2026-02-23 00:06:02 +0900

    fix: 누락된 쿼리 키 추가

--- src/entities/product/model/keys.ts
+++ src/entities/product/model/keys.ts
@@ -1,7 +1,8 @@
 export const productKeys = {
   all: ['product'] as const,
   lists: () => [...productKeys.all, 'list'] as const,
-  list: (accountname: string) => [...productKeys.lists(), accountname] as const,
+  list: (accountname: string, limit?: number, skip?: number) =>
+    [...productKeys.lists(), accountname, limit, skip] as const,
   details: () => [...productKeys.all, 'detail'] as const,
   detail: (productId: string) => [...productKeys.details(), productId] as const,
 };
