acff4e0e73cfde7073120e1a9a245ed77a19eba0
Catailog <tlsdudghks11@gmail.com>
2026-02-23 11:23:02 +0900

    feat: assets export 추가

---
+++ src/shared/assets/index.ts
@@ -0,0 +1,21 @@
+import facebookIcon from './icons/facebook-icon.svg';
+import googleIcon from './icons/google-icon.svg';
+import kakaoIcon from './icons/kakao-icon.svg';
+import fullLogoAlyac404 from './images/full-logo-alyac-404.png';
+import fullLogoAlyacGray from './images/full-logo-alyac-gray.png';
+import fullLogoAlyacNoText from './images/full-logo-alyac-no-text.png';
+import fullLogoAlyacPng from './images/full-logo-alyac-png.png';
+import uploadImage from './images/upload-image.png';
+import uploadSmallImage from './images/upload-small-image.png';
+
+export {
+  facebookIcon,
+  googleIcon,
+  kakaoIcon,
+  fullLogoAlyac404,
+  fullLogoAlyacGray,
+  fullLogoAlyacNoText,
+  fullLogoAlyacPng,
+  uploadImage,
+  uploadSmallImage,
+};
