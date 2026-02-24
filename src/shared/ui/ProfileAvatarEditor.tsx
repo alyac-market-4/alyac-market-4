e028859a2d748245aef484780a67830d0546c4c0
Catailog <tlsdudghks11@gmail.com>
2026-02-24 23:35:24 +0900

    refactor: Avatar 코드를 ProfileAvatar로 대체

--- src/shared/ui/ProfileAvatarEditor.tsx
+++ src/shared/ui/ProfileAvatarEditor.tsx
@@ -1,22 +1,16 @@
 import { ImageFileButton } from '@/shared/ui/ImageFileButton';
-
-import { Avatar, AvatarFallback, AvatarImage } from './avatar';
+import { ProfileAvatar } from '@/shared/ui/ProfileAvatar';
 
 interface ProfileAvatarProps {
-  src: string;
+  src?: string;
   alt: string;
   size?: 'default' | 'sm' | 'lg' | 'xl';
 }
 
-export const ProfileAvatarEditor = ({ src, alt, size = 'default' }: ProfileAvatarProps) => {
+export const ProfileAvatarEditor = ({ src, alt, size = 'xl' }: ProfileAvatarProps) => {
   return (
     <div className="relative inline-block">
-      <Avatar size={size}>
-        <AvatarImage alt={alt} src={src} />
-        <AvatarFallback>
-          <img alt={alt} src={src} />
-        </AvatarFallback>
-      </Avatar>
+      <ProfileAvatar src={src} alt={alt} size={size} />
       <ImageFileButton className="absolute right-0 bottom-0" />
     </div>
   );
