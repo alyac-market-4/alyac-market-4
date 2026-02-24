1ac9be52bc2a818753064b221844d31b534ac501
Catailog <tlsdudghks11@gmail.com>
2026-02-24 21:14:56 +0900

    feat: 댓글 삭제 기능 추가

--- src/pages/post-detail/ui/CommentDetail.tsx
+++ src/pages/post-detail/ui/CommentDetail.tsx
@@ -1,4 +1,5 @@
 import { useCommentMutation } from '@/entities/comment';
+import { useConfirmDialog } from '@/shared/lib/confirm-dialog/useConfirmDialog';
 import type { Comment } from '@/shared/model';
 import { KebabMenu, ProfileAvatar } from '@/shared/ui';
 
@@ -9,6 +10,7 @@ interface CommentDetailProps {
 
 export default function CommentDetail({ postId, comment }: CommentDetailProps) {
   const { deleteMutation, reportMutation } = useCommentMutation();
+  const { openConfirm, closeConfirm } = useConfirmDialog();
 
   return (
     <>
@@ -32,7 +34,13 @@ export default function CommentDetail({ postId, comment }: CommentDetailProps) {
               {
                 label: '삭제',
                 onClick: () => {
-                  deleteMutation.mutate({ postId, commentId: comment.id });
+                  openConfirm({
+                    title: '정말 삭제하시겠습니까?',
+                    description: '삭제된 게시물은 복구할 수 없습니다.',
+                    actionText: '삭제',
+                    onConfirm: () => deleteMutation.mutate({ postId, commentId: comment.id }),
+                    onCancel: closeConfirm,
+                  });
                 },
               },
             ]}
