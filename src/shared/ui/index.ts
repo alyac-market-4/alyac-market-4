af62cc44d3d676f2af509808fa7f76af04857b85
Catailog <tlsdudghks11@gmail.com>
2026-02-25 17:43:24 +0900

    fix: 충돌 코드 해결

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -9,12 +9,8 @@ export * from './ImageFileButton';
 export * from './input-group';
 export * from './KebabMenu';
 export * from './LoadingState';
+export * from './PostImage';
 export * from './ProfileAvatar';
 export * from './ProfileAvatarEditor';
 export * from './SuspenseWrapper';
-<<<<<<< HEAD
-
-export * from './PostImage';
-=======
 export * from './toggle-group';
->>>>>>> 41fa16e37f527442cce329397bbd5003e592024e
