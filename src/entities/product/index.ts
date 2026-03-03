0bdbd2abbd82dce8a02969f1860e47cb7a7b6917
Catailog <tlsdudghks11@gmail.com>
2026-03-03 22:00:32 +0900

    feat: ProductCard 스켈레톤 UI 추가

--- src/entities/product/index.ts
+++ src/entities/product/index.ts
@@ -2,3 +2,4 @@ export * from './api/productApi';
 export * from './api/useProductQuery';
 export * from './model/types';
 export * from './ui/ProductCard';
+export * from './ui/ProductCardSkeleton';
