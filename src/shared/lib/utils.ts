5ae5e20c80cc27725a044f006b3c7779bf77be05
Catailog <tlsdudghks11@gmail.com>
2026-03-03 23:29:42 +0900

    feat: replace 히스토리를 관리해주는 useReplaceNavigate 추가

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from 'clsx';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
@@ -7,4 +8,19 @@ export function cn(...inputs: ClassValue[]) {
 
 export const formatCurrency = (value: number): string => {
   return `${new Intl.NumberFormat('ko-KR').format(value)}원`;
+};
+
+export const useReplaceNavigate = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const navigateBackOrTo = (fallbackPath: string) => {
+    if (location.key !== 'default') {
+      navigate(-1);
+    } else {
+      navigate(fallbackPath, { replace: true });
+    }
+  };
+
+  return { navigateBackOrTo };
 };
