44f01ebee60bf2d558a860091d9d38eab3b15117
Catailog <tlsdudghks11@gmail.com>
2026-03-04 17:38:12 +0900

    feat: 버튼 1개 dialog 추가

--- src/shared/ui/alert-dialog.tsx
+++ src/shared/ui/alert-dialog.tsx
@@ -75,10 +75,7 @@ function AlertDialogFooter({ className, ...props }: React.ComponentProps<'div'>)
   return (
     <div
       data-slot="alert-dialog-footer"
-      className={cn(
-        'flex flex-col-reverse gap-2 group-data-[size=sm]/alert-dialog-content:grid group-data-[size=sm]/alert-dialog-content:grid-cols-2 sm:flex-row sm:justify-end',
-        className,
-      )}
+      className={cn('flex flex-col-reverse gap-2 sm:flex-row sm:justify-end', className)}
       {...props}
     />
   );
