571acfd82c62c0afd37c6a27f3e9283ec64767c5
Catailog <tlsdudghks11@gmail.com>
2026-03-05 16:49:20 +0900

    refactor: index.css 불필요한 코드 제거, nav 스타일 변경

--- src/widgets/nav/ui/Nav.tsx
+++ src/widgets/nav/ui/Nav.tsx
@@ -7,7 +7,7 @@ export const Nav = () => {
   const pathname = useLocation().pathname;
 
   return (
-    <nav className="bg-background sticky right-0 bottom-0 left-0 border-t">
+    <nav className="bg-sidebar text-sidebar-foreground sticky right-0 bottom-0 left-0 border-t">
       <div className="flex items-center justify-around py-2">
         <NavItem to="/feed" icon={<House />} label="홈" isActive={pathname === '/feed'} />
         <NavItem to="/chat" icon={<MessageCircle />} label="채팅" isActive={pathname === '/chat'} />
