e0a1569afe7ecbded00591a563e4ee4d4b86d699
Catailog <tlsdudghks11@gmail.com>
2026-03-06 17:06:28 +0900

    refactor: 통신량/시간 조정

--- src/entities/post/hooks/useAllPosts.ts
+++ src/entities/post/hooks/useAllPosts.ts
@@ -4,7 +4,7 @@ import { postKeys } from '@/shared/model';
 
 import { postApi } from '../api/postApi';
 
-export const useAllPosts = (limit: number = 10, skip: number = 0) => {
+export const useAllPosts = (limit: number = 5, skip: number = 0) => {
   return useQuery({
     queryKey: postKeys.list(limit, skip),
     queryFn: () => postApi.getAllPosts(limit, skip),
