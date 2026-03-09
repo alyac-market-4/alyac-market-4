ab68cdaaf54ed9ba94a8665446dbba0a7b11b8f7
Catailog <tlsdudghks11@gmail.com>
2026-03-09 14:35:29 +0900

    fix: 좋아요 취소가 안 되던 현상 수정

--- src/features/post/ui/LikeButton.tsx
+++ src/features/post/ui/LikeButton.tsx
@@ -1,6 +1,6 @@
 import { Heart } from 'lucide-react';
 
-import { useToggleLikePost } from '@/entities/post';
+import { useToggleLikePost, useUnlikePost } from '@/entities/post';
 import { cn } from '@/shared/lib';
 
 interface LikeButtonProps {
@@ -11,10 +11,11 @@ interface LikeButtonProps {
 
 export const LikeButton = ({ postId, heartCount, hearted }: LikeButtonProps) => {
   const { mutate: toggleLikePost } = useToggleLikePost();
+  const { mutate: unlikePost } = useUnlikePost();
 
   return (
     <button
-      onClick={() => toggleLikePost(postId)}
+      onClick={() => (!hearted ? toggleLikePost(postId) : unlikePost(postId))}
       className={cn(
         'ring-offset-background focus-visible:ring-ring hover:text-accent-foreground inline-flex h-auto cursor-pointer items-center justify-center gap-1.5 rounded-md p-0 text-sm font-medium whitespace-nowrap transition-colors hover:bg-transparent focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:shrink-0',
         hearted ? 'text-red-500' : '',
