673d98ddae228596dba22c58f1c1c69f6fb29e74
Catailog <tlsdudghks11@gmail.com>
2026-03-09 13:15:21 +0900

    fix: 테마가 항상 HomePage에서 system으로 바뀌는 현상 수정

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,15 +1,12 @@
 import { useEffect, useState } from 'react';
 
 import { SplashView } from '@/features/splash';
-import { useThemeStore } from '@/shared/lib';
 import { AuthSection } from '@/widgets/auth';
 
 export const HomePage = () => {
   const [showSplash, setShowSplash] = useState(true);
-  const { initThemeByLocalStorage } = useThemeStore();
 
   useEffect(() => {
-    initThemeByLocalStorage();
     const timer = setTimeout(() => setShowSplash(false), 2000);
     return () => clearTimeout(timer);
     // eslint-disable-next-line react-hooks/exhaustive-deps
